8903d1f71eb4093b2909719037a53f3b07449118
Saravana16041999 <saravanakumar16041999@gmail.com>
2022-09-28 14:15:49 +0530

    completed-full-project

--- script/script.js
+++ script/script.js
@@ -2,15 +2,39 @@
 
 const movieEl = document.getElementById('Movie')
 
-let Moneyel = movieEl.value; 
+let tecketpriceEl = movieEl.value; 
 
 // need to select what movie watching 
 // select the seat for booking
 // calculate the amount and how much for each seat.
-const seats = document.getElementById('screen')
+const seatsEl = document.getElementById('screen');
 
-const count = document.getElementById('count')
+const count = document.getElementById('count');
 
-const total = document.getElementById('total')
+const total = document.getElementById('total');
 
-console.log(seats)
+let seatCountEl = 0;
+
+// function for calculation
+function calculUi(){
+    count.innerText = seatCountEl
+    total.innerText = seatCountEl * tecketpriceEl
+    
+}
+
+
+movieEl.addEventListener('change',()=>{
+    tecketpriceEl = movieEl.value
+    calculUi()
+})
+seatsEl.addEventListener('click',(e)=>{
+    if(e.target.classList.contains('seat')){
+        if(!e.target.classList.contains('Ocupied')){
+            e.target.classList.toggle('selected')
+            const selectedSeatEl = document.querySelectorAll('.row .selected')
+            seatCountEl =  selectedSeatEl.length
+            calculUi()
+        }
+
+    }
+})
