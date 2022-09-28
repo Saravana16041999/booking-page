f5dcc7689568cffb8f859dedc72493a545b84162
Saravana16041999 <saravanakumar16041999@gmail.com>
2022-09-28 13:32:53 +0530

    updated html

--- script/script.js
+++ script/script.js
@@ -1,5 +1,16 @@
 `use strict`;
+
 const movieEl = document.getElementById('Movie')
 
 let Moneyel = movieEl.value; 
 
+// need to select what movie watching 
+// select the seat for booking
+// calculate the amount and how much for each seat.
+const seats = document.getElementById('screen')
+
+const count = document.getElementById('count')
+
+const total = document.getElementById('total')
+
+console.log(seats)
